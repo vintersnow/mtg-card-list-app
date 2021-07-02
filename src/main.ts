6c4771ec83a0e207fdf288b1b7069af810397f5e
vinter <ankasandora@gmail.com>
2021-07-02 13:55:57 +0900

    add ui-components

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,12 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-createApp(App).use(router).mount("#app");
+import PrimeVue from "primevue/config";
+
+const app = createApp(App);
+
+app.use(PrimeVue);
+
+app.use(router);
+
+app.mount("#app");
