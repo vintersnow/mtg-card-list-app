c74980e9a4e1843dac82a80bc706faac9037c4e9
vinter <ankasandora@gmail.com>
2021-07-03 17:29:19 +0900

    change favicon

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   publicPath:
     process.env.NODE_ENV === "production" ? "/mtg-card-list-app/" : "/",
+  pages: {
+    index: {
+      entry: 'src/main.ts',
+      title: "MtG Cards",
+    }
+  }
 };
