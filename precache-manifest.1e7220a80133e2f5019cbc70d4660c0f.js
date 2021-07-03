a9d9297f088caa8bde93aa3d1053d8dcddffd442
vintersnow <vintersnow@users.noreply.github.com>
2021-07-03 08:44:53 +0000

    deploy: 97d054cb3b7aba06b86dbfb010568d59e242167c

---
+++ precache-manifest.1e7220a80133e2f5019cbc70d4660c0f.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fae49842a70c220fbfeb",
+    "url": "/mtg-card-list-app/css/chunk-vendors.d789dbc4.css"
+  },
+  {
+    "revision": "530ef1507cda1560d60d",
+    "url": "/mtg-card-list-app/css/index.181ba270.css"
+  },
+  {
+    "revision": "0d15f9c4f194781095593dfcecacd735",
+    "url": "/mtg-card-list-app/data/20210701.json"
+  },
+  {
+    "revision": "1ba2ae710d927f13d483fd5d1e548c9b",
+    "url": "/mtg-card-list-app/favicon.ico.bk"
+  },
+  {
+    "revision": "121254f73060bcbb53ca13258dbd134f",
+    "url": "/mtg-card-list-app/fonts/primeicons.121254f7.ttf"
+  },
+  {
+    "revision": "3b6e3706f42d8876fe364ab4d75683fd",
+    "url": "/mtg-card-list-app/fonts/primeicons.3b6e3706.woff"
+  },
+  {
+    "revision": "b0f5d02f4e70967dcc8162e938484366",
+    "url": "/mtg-card-list-app/fonts/primeicons.b0f5d02f.eot"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "/mtg-card-list-app/img/color.c7a33805.png"
+  },
+  {
+    "revision": "25954ab5e32c01d577dcafd035597f5d",
+    "url": "/mtg-card-list-app/img/primeicons.25954ab5.svg"
+  },
+  {
+    "revision": "24bb0bb3c2f582440fa391bbb10abafc",
+    "url": "/mtg-card-list-app/index.html"
+  },
+  {
+    "revision": "a9e2f9bfa2cd443bf97f",
+    "url": "/mtg-card-list-app/js/about.f1a30d31.js"
+  },
+  {
+    "revision": "fae49842a70c220fbfeb",
+    "url": "/mtg-card-list-app/js/chunk-vendors.d6b72e64.js"
+  },
+  {
+    "revision": "530ef1507cda1560d60d",
+    "url": "/mtg-card-list-app/js/index.91323548.js"
+  },
+  {
+    "revision": "9d8ee20a81073fa1fba4c790cc035af4",
+    "url": "/mtg-card-list-app/logo.png"
+  },
+  {
+    "revision": "ad0e01075c9babeb5116b823bb7bedfd",
+    "url": "/mtg-card-list-app/logo.png:Zone.Identifier"
+  },
+  {
+    "revision": "cdff9fbfe1332d29e0707c931a40ea2f",
+    "url": "/mtg-card-list-app/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/mtg-card-list-app/robots.txt"
+  }
+]);
