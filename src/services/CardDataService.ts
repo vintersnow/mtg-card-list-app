6a5d6dcfd0f11ee12f489db8e66215bd2814a156
vinter <ankasandora@gmail.com>
2021-07-03 12:09:53 +0900

    add data

--- src/services/CardDataService.ts
+++ src/services/CardDataService.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 import { CardDetail } from "@/models";
 
+const fileName = '20210701.json'
+
 export default class CardListService {
   async getCardList(): Promise<CardDetail[]> {
-    return (await axios.get("data/card_list.json")).data;
+    return (await axios.get(`data/${fileName}`)).data;
   }
 }
